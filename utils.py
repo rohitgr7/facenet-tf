733c12bef7033220915b79a4e7ac6b510f61f1a6
rohitgr7 <rohitgr1998@gmail.com>
2018-09-13 18:48:04 +0530

    Added seperate returns for images and rectangles

--- utils.py
+++ utils.py
@@ -3,46 +3,55 @@ from PIL import Image
 import dlib
 import openface
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import os
 
 
 def load_image(image_path):
     return np.array(Image.open(image_path))
 
 
-def detect_and_align(image_paths, image_size, training=True):
+def detect_and_align(images, image_size, meta_dir, training=True):
     img_faces = []
+    img_rects = []
 
-    for img_path in image_paths:
-        img = load_image(img_path)
+    for image in images:
+        if os.path.isfile(image):
+            img = load_image(image)
+        else:
+            img = image
 
-        predictor_model = './meta/shape_predictor_68_face_landmarks.dat'
+        predictor_model = os.path.join(meta_dir, 'shape_predictor_68_face_landmarks.dat')
         face_detector = dlib.get_frontal_face_detector()
         face_aligner = openface.AlignDlib(predictor_model)
 
         detected_faces = face_detector(img, 1)
 
         faces = []
+        rects = []
 
-        for i, face_rect in enumerate(detected_faces):
+        for face_rect in detected_faces:
 
             aligned_face = face_aligner.align(image_size, img, face_rect, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
 
+            faces.append(aligned_face)
+
             if training:
-                faces.append(aligned_face)
                 break
-            else:
-                faces.append((aligned_face, face_rect))
+
+            rects.append(face_rect)
 
         img_faces.append(faces)
+        img_rects.append(rects)
 
-    return img_faces
+    return img_faces, img_rects
 
 
 def plot_image(img):
     plt.imshow(img / 255.)
 
 
-def load_model(model_path, input_map=None):
+def _load_model(model_path, input_map=None):
     if os.path.isfile(model_path):
         with tf.gfile.FastGFile(model_path, 'rb') as f:
             graph_def = tf.GraphDef()
@@ -64,3 +73,14 @@ def get_model_filenames(model_dir):
     if ckpt and ckpt.model_checkpoint_path:
         ckpt_file = os.path.basename(ckpt.model_checkpoint_path)
         return meta_file, ckpt_file
+
+
+def get_model_tensors(model_path):
+    _load_model(model_path)
+    print('Embedding model loaded')
+
+    input_placeholder = tf.get_default_graph().get_tensor_by_name('import/embeddings:0')
+    embeddings_tensor = tf.get_default_graph().get_tensor_by_name('import/embeddings:0')
+    phase_train_placeholder = tf.get_default_graph().get_tensor_by_name('import/phase_train:0')
+
+    return input_placeholder, embeddings_tensor, phase_train_placeholder

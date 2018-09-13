9ade600ddbd0b078deeb3e747106e71421d2e363
rohitgr7 <rohitgr1998@gmail.com>
2018-09-13 17:20:41 +0530

    Added embedding load_model function

--- utils.py
+++ utils.py
@@ -40,3 +40,27 @@ def detect_and_align(image_paths, image_size, training=True):
 
 def plot_image(img):
     plt.imshow(img / 255.)
+
+
+def load_model(model_path, input_map=None):
+    if os.path.isfile(model_path):
+        with tf.gfile.FastGFile(model_path, 'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+
+        tf.import_graph_def(graph_def, input_map)
+
+    else:
+        meta_file, ckpt_file = get_model_filenames(model_path)
+        saver = tf.train.import_meta_graph(os.path.join(model_path, meta_file), input_map=input_map)
+        saver.restore(tf.get_default_session(), os.path.join(model_path, meta_file))
+
+
+def get_model_filenames(model_dir):
+    files = os.listdir(model_dir)
+    meta_file = [f for f in files if f.endswith('.meta')][0]
+    ckpt = tf.train.get_checkpoint_state(model_dir)
+
+    if ckpt and ckpt.model_checkpoint_path:
+        ckpt_file = os.path.basename(ckpt.model_checkpoint_path)
+        return meta_file, ckpt_file
